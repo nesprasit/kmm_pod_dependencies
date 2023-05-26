66154cee8a126753a8f137a91af179cedc258152
Nes <Prasit.s@clicknext.com>
2023-05-27 00:31:05 +0700

    pod

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "shared",
-    platforms: [.iOS(.v13)]
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "shared",targets: ["shared"]),
         .library(name: "dependency",targets: ["dependency"]),
